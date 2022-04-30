c8425a231b9f4f28e11cb68006387a3e4d34a7eb
kantagonist <afk24rpct@protonmail.com>
2022-04-30 22:37:04 +0200

    boyscout

--- src/main/kotlin/configbuilder/ConfigBuilder.kt
+++ src/main/kotlin/configbuilder/ConfigBuilder.kt
@@ -9,44 +9,34 @@ import java.nio.file.Files
 import kotlin.io.path.Path
 
 /**
- * Reads the given file and returns it as a usable virtual object
+ * Reads the yaml config file and creates a virtual representation.
+ *
+ * @param yamlConfigFilePath the absolute path of the config file as a String
+ *
+ * @return An array of FromTo pairings
  */
-internal class ConfigBuilder {
+@kotlin.jvm.Throws(DotStringsTranslatorException::class)
+internal fun readConfig(yamlConfigFilePath: String): VirtualConfigFile {
 
-    companion object {
-
-        /**
-         * Reads the yaml config file
-         *
-         * @param yamlConfigFilePath the absolute path of the config file as a String
-         *
-         * @return An array of FromTo pairings
-         */
-        @JvmStatic
-        @kotlin.jvm.Throws(DotStringsTranslatorException::class)
-        fun readConfig(yamlConfigFilePath: String): VirtualConfigFile {
-
-            // check yaml file
-            val yamlFile = File(yamlConfigFilePath)
-            if (!yamlFile.exists() || !yamlFile.isFile) {
-                throw DotStringsTranslatorException(
-                    tag = "[FILE NOT FOUND]",
-                    message = "Could not find config file under:\n$yamlConfigFilePath"
-                )
-            }
-
-            // use SnakeYaml to get the contents
-            val mapper = ObjectMapper(YAMLFactory())
-            mapper.registerModule(KotlinModule.Builder().build())
-            val result = Files.newBufferedReader(Path(yamlConfigFilePath)).use {
-                mapper.readValue(yamlFile, VirtualConfigFile::class.java)
-            }
-
-            // fix given rootPath to account for relative and absolute path
-            if (result.rootPath[0] == '.') {
-                result.rootPath =  yamlConfigFilePath + result.rootPath.substring(1)
-            }
-            return result
-        }
+    // check yaml file
+    val yamlFile = File(yamlConfigFilePath)
+    if (!yamlFile.exists() || !yamlFile.isFile) {
+        throw DotStringsTranslatorException(
+            tag = "[FILE NOT FOUND]",
+            message = "Could not find config file under:\n$yamlConfigFilePath"
+        )
     }
+
+    // use SnakeYaml to get the contents
+    val mapper = ObjectMapper(YAMLFactory())
+    mapper.registerModule(KotlinModule.Builder().build())
+    val result = Files.newBufferedReader(Path(yamlConfigFilePath)).use {
+        mapper.readValue(yamlFile, VirtualConfigFile::class.java)
+    }
+
+    // fix given rootPath to account for relative and absolute path
+    if (result.rootPath[0] == '.') {
+        result.rootPath =  yamlConfigFilePath + result.rootPath.substring(1)
+    }
+    return result
 }
