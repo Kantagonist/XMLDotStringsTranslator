5d743a43fa3e3ccfce71adf238d29d4242c16e13
kantagonist <afk24rpct@protonmail.com>
2022-05-11 21:06:20 +0200

    map merger, already debugged

---
+++ src/main/kotlin/utility/MapMerge.kt
@@ -0,0 +1,48 @@
+package utility
+
+import inputmapcreators.NameContentTuple
+
+/**
+ * Merges the given Mappings via FULL OUTER JOINS.
+ * In case of duplicates, the first occurrence in the list of mappings is used.
+ * The latter ones are discarded.
+ *
+ * @param mappings the list of mappings to merge
+ *
+ * @return a merged mapping with first come, first served for duplicate entries
+ */
+internal fun mergeMappings(mappings: List<List<NameContentTuple>>): List<NameContentTuple> {
+    var mergedMapping = listOf<NameContentTuple>()
+    for (currentMapping in mappings) {
+        mergedMapping = mergeMappings(mergedMapping, currentMapping)
+    }
+    return mergedMapping
+}
+
+/**
+ * Merges the given top mapping with the given bottom mapping.
+ * The merge rule is TOP FIRST.
+ * This is similar to a FULL OUTER JOIN.
+ * The resources, which exist in both are taken from the top input.
+ *
+ * @param topMapping the dominant mapping (its duplicates are used)
+ * @param bottomMapping the subservient mapping (its duplicates are overwritten by the instances in the top mapping)
+ *
+ * @return the merged mapping
+ */
+private fun mergeMappings(topMapping: List<NameContentTuple>, bottomMapping: List<NameContentTuple>): List<NameContentTuple> {
+    val result = mutableListOf<NameContentTuple>()
+    val mutableBottomMapping = bottomMapping.toMutableList()
+    topMapping.forEach { topTuple ->
+        result.add(topTuple)
+        mutableBottomMapping.find { bottomTuple ->
+            topTuple.name == bottomTuple.name
+        }?.let {
+            mutableBottomMapping.remove(it)
+        }
+    }
+    mutableBottomMapping.forEach {
+        result.add(it)
+    }
+    return result
+}
---
+++ src/test/kotlin/MapMergeTests.kt
@@ -0,0 +1,122 @@
+import inputmapcreators.NameContentTuple
+import org.junit.jupiter.api.Test
+import utility.mergeMappings
+import kotlin.test.assertEquals
+
+class MapMergeTests {
+
+    @Test
+    fun mergeSmallExclusiveMaps() {
+
+        // create inputs
+        val topInput = listOf(
+            NameContentTuple("hello","world"),
+            NameContentTuple("12", "34")
+        )
+        val bottomInput = listOf(
+            NameContentTuple("this", "test is"),
+            NameContentTuple("an_easy_one", "TO do in my IDE")
+        )
+
+        // create expected output
+        val expectedOutput = listOf(
+            NameContentTuple("hello","world"),
+            NameContentTuple("12", "34"),
+            NameContentTuple("this", "test is"),
+            NameContentTuple("an_easy_one", "TO do in my IDE")
+        )
+
+        // merge input lists
+        val actualOutput = mergeMappings(listOf(topInput, bottomInput))
+
+        // compare results
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun mergeTwoInclusiveMaps() {
+
+        // create inputs
+        val topInput = listOf(
+            NameContentTuple("hello","world"),
+            NameContentTuple("12", "34"),
+            NameContentTuple("hello_my_freedom", "cry_out_for_a_hero") ,
+            NameContentTuple("1444442", "34"),
+            NameContentTuple("this_test_is_top", "45820582")
+        )
+        val bottomInput = listOf(
+            NameContentTuple("this", "test is"),
+            NameContentTuple("this_test_is_top", "00000000000000"),
+            NameContentTuple("an_easy_one", "TO do in my IDE"),
+            NameContentTuple("hello_my_freedom", "this should not be part of it")
+        )
+
+        // create expected output
+        val expectedOutput = listOf(
+            NameContentTuple("hello","world"),
+            NameContentTuple("12", "34"),
+            NameContentTuple("hello_my_freedom", "cry_out_for_a_hero") ,
+            NameContentTuple("1444442", "34"),
+            NameContentTuple("this_test_is_top", "45820582"),
+            NameContentTuple("this", "test is"),
+            NameContentTuple("an_easy_one", "TO do in my IDE")
+        )
+
+        // merge input lists
+        val actualOutput = mergeMappings(listOf(topInput, bottomInput))
+
+        // compare results
+        assertEquals(expectedOutput, actualOutput)
+    }
+
+    @Test
+    fun mergeMultipleInclusiveMaps() {
+
+        // create inputs
+        val input1 = listOf(
+            NameContentTuple("hello","world"),
+            NameContentTuple("12", "34"),
+            NameContentTuple("hello_my_freedom", "cry_out_for_a_hero") ,
+            NameContentTuple("1444442", "34"),
+            NameContentTuple("this_test_is_top", "45820582")
+        )
+        val input2 = listOf(
+            NameContentTuple("this", "test is"),
+            NameContentTuple("an_easy_one", "TO do in my IDE"),
+            NameContentTuple("hello_my_freedom", "this should not be part of it")
+        )
+        val input3 = listOf(
+            NameContentTuple("this_test_is_top", "22222222"),
+            NameContentTuple("new entry", "ranging vows"),
+            NameContentTuple("duplicate_of_4", "dominant text")
+        )
+        val input4 = listOf(
+            NameContentTuple("duplicate_of_4", "subservient text"),
+            NameContentTuple("something_else", "backpack, y soy la hora"),
+            NameContentTuple("The smartest clipper you will find", "its ho hey ho"),
+            NameContentTuple("Shes the Margaret Evans of", "of the blue star line")
+        )
+
+        // create expected output
+        val expectedOutput = listOf(
+            NameContentTuple("hello","world"),
+            NameContentTuple("12", "34"),
+            NameContentTuple("hello_my_freedom", "cry_out_for_a_hero") ,
+            NameContentTuple("1444442", "34"),
+            NameContentTuple("this_test_is_top", "45820582"),
+            NameContentTuple("this", "test is"),
+            NameContentTuple("an_easy_one", "TO do in my IDE"),
+            NameContentTuple("new entry", "ranging vows"),
+            NameContentTuple("duplicate_of_4", "dominant text"),
+            NameContentTuple("something_else", "backpack, y soy la hora"),
+            NameContentTuple("The smartest clipper you will find", "its ho hey ho"),
+            NameContentTuple("Shes the Margaret Evans of", "of the blue star line")
+        )
+
+        // merge input lists
+        val actualOutput = mergeMappings(listOf(input1, input2, input3, input4))
+
+        // compare results
+        assertEquals(expectedOutput, actualOutput)
+    }
+}
