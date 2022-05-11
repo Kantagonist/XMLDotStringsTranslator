59e101cd613dd26d20b7a819c1f6629e6888f3d7
kantagonist <afk24rpct@protonmail.com>
2022-05-11 20:02:16 +0200

    cleaned Yamltests

--- src/test/kotlin/YamlConfigReadingTests.kt
+++ src/test/kotlin/YamlConfigReadingTests.kt
@@ -1,23 +1,53 @@
 import configbuilder.Translation
 import configbuilder.VirtualConfigFile
 import configbuilder.readConfig
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
 
 class YamlConfigReadingTests {
 
-    @Test
-    fun testFileExistence() {
-        val path = "${System.getProperty("user.dir")}/src/test/resources/XmlDotStringConfig.yaml"
-        assert(File(path).exists())
+    companion object {
+
+        @AfterAll
+        @JvmStatic
+        fun cleanUp() {
+            val inputFile = File("${System.getProperty("user.dir")}/src/test/resources/MappingConfig.yaml")
+            if (inputFile.exists()) {
+                inputFile.delete()
+            }
+        }
     }
 
     @Test
     fun testYamlReading() {
+
+        // create input file
+        val inputFilePath = "${System.getProperty("user.dir")}/src/test/resources/MappingConfig.yaml"
+        val inputFile = File(inputFilePath)
+        inputFile.createNewFile()
+        inputFile.writeText(
+            "# paths relative to this yaml file\n" +
+                    "rootPath: \".\"\n" +
+                    "translations:\n" +
+                    "  - from:\n" +
+                    "      - \"ThisFile.xml\"\n" +
+                    "      - \"ThisOtherFile.xml\"\n" +
+                    "    to:\n" +
+                    "      - \"NewTestFile.strings\"\n" +
+                    "      - \"OtherTestFile.strings\"\n" +
+                    "  - from:\n" +
+                    "      - \"MyFile.xml\"\n" +
+                    "      - \"MyOtherFile.xml\"\n" +
+                    "    to:\n" +
+                    "      - \"MyTestFile.strings\"\n" +
+                    "      - \"MyOtherTestFile.strings\""
+        )
+
         // expected output
         val expectedConfig = VirtualConfigFile(
-            rootPath = "${System.getProperty("user.dir")}/src/test/resources/XmlDotStringConfig.yaml",
+            rootPath = "${System.getProperty("user.dir")}/src/test/resources/MappingConfig.yaml",
             translations = listOf(
                 Translation(
                     from = listOf(
@@ -42,9 +72,8 @@ class YamlConfigReadingTests {
             )
         )
 
-        // testing
-        val path = "${System.getProperty("user.dir")}/src/test/resources/XmlDotStringConfig.yaml"
-        val actualConfig = readConfig(path)
+        // read in config
+        val actualConfig = readConfig(inputFilePath)
 
         // checking results
         assertEquals(expectedConfig, actualConfig)
